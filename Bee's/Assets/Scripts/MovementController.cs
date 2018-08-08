f21d89b5bd0f32dc883139f5408b55152161023e
Charles Cozart <charlescozart24@gmail.com>
2018-08-07 17:03:52 -0700

    working on video

--- Bee's/Assets/Scripts/MovementController.cs
+++ Bee's/Assets/Scripts/MovementController.cs
@@ -161,6 +161,7 @@ public class MovementController : MonoBehaviour
         {
             if(IsMoving)
             {
+                Debug.Log("Leaving Stealth");
                 IsStealth = false;
                 return;
             }
